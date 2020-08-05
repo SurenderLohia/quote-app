a8e61e84f91ac108f9aa1eaa96603413eb755f5b
Surender Lohia <surender.lohia.e@gmail.com>
2020-08-05 08:15:03 +0530

    Add Loader and CNAME

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteListComponent } from './quote-list/quote-list.component';
 import { AddQuoteComponent } from './add-quote/add-quote.component';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       QuoteListComponent,
-      AddQuoteComponent
+      AddQuoteComponent,
+      LoaderComponent
    ],
    imports: [
       BrowserModule,
